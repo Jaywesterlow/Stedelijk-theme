f701241ffd883b3ca3ef394e044759303eca556f
Jaywesterlow <jaywesterlow71@gmail.com>
2022-01-22 19:31:00 +0100

    Updated up to program 1 - Added images

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 
 <div class="w-100 d-flex align-items-center flex-row header-spacing">
   <div class="header-container d-flex align-items-center flex-row">
-    <h2>WORKSHOP BOUW ZOALS DE MEESTERS</h2>
-    <i class="fas fa-long-arrow-alt-right fa-3x arrow"></i>
+    <h3>WORKSHOP BOUW ZOALS DE MEESTERS</h3>
+    <i class="fas fa-long-arrow-alt-right fa-3x arrow-right"></i>
   </div>
 </div>
 
@@ -18,8 +18,8 @@
 
 <div class="w-100 d-flex align-items-center flex-row header-spacing">
   <div class="header-container d-flex align-items-center flex-row">
-    <h2>PROGRAMMA 1</h2>
-    <i class="fas fa-long-arrow-alt-right fa-3x arrow"></i>
+    <h3>PROGRAMMA 1</h3>
+    <i class="fas fa-long-arrow-alt-right fa-3x arrow-right"></i>
   </div>
 </div>
 
@@ -27,8 +27,8 @@
 
 <div class="w-100 d-flex align-items-center flex-row header-spacing">
   <div class="header-container d-flex align-items-center flex-row">
-    <h2>PROGRAMMA 2</h2>
-    <i class="fas fa-long-arrow-alt-right fa-3x arrow"></i>
+    <h3>PROGRAMMA 2</h3>
+    <i class="fas fa-long-arrow-alt-right fa-3x arrow-right"></i>
   </div>
 </div>
 
@@ -36,8 +36,8 @@
 
 <div class="w-100 d-flex align-items-center flex-row header-spacing">
   <div class="header-container d-flex align-items-center flex-row">
-    <h2>PROGRAMMA 3</h2>
-    <i class="fas fa-long-arrow-alt-right fa-3x arrow"></i>
+    <h3>PROGRAMMA 3</h3>
+    <i class="fas fa-long-arrow-alt-right fa-3x arrow-right"></i>
   </div>
 </div>
 
@@ -58,9 +58,97 @@
 
 <hr class="divider-py">
 
-
+<div class="w-100 d-flex align-items-center flex-row header-spacing">
+  <button type="button" class="btn bg-dark text-light my-3">Koop Tickets</button>
+</div>
 
 <hr class="divider-py">
+
+<div class="w-100 d-flex flex-column flex-xl-row align-items-start justify-content-around justify-content-xl-between header-spacing">
+  <div class="programExplenation">
+    <p class="description">
+      Is het mogelijk om een actieve dag in het stedelijk museum te hebben voor een grote groep? <br>
+      Ja het kan:
+    </p>
+    <h4 class="pt-2">Een dag voor het leven</h4>
+    <p class="description">
+      Voor groepen tot 200 personen kunt u kunst ervaren vanuit de ogen van kunstenaars. <br>
+      Kies 1 van onze dag activeit pakketten die past bij uw wensen en kom langs in ons museum. <br>
+      Bestaande uit: <br>
+    </p>
+    <ul class="paragraphList">
+      <li>- Een rondleiding met gids</li>
+      <li>- Een uitgebreide lunch</li>
+      <li>- De bouw workshop</li>
+    </ul>
+    <br>
+    <p class="description">
+      Behoefte aan een kunstzinnige maar actieve dag? <br>
+      Koop dan nu tickets en kom langs!
+    </p>
+    <br>
+    <em>
+      Wij zien u graag voor een dagje van kijken en doen bij de kunstworkshop.
+    </em>
+  </div>
+  
+  <div class="d-flex justify-content-xl-end programExplenation">
+    <img class="artwork-1" src="<?php echo get_template_directory_uri(); ?>/images/artwork1.png" />
+  </div>
+</div>
+
+<hr class="divider-py mt-4">
+
+<div class="header-spacing">
+  
+</div>
+
+<div class="w-100 d-flex justify-content-center flex-column header-spacing">
+  <h1 class="program">PROGRAMMA 1</h1>
+  <h2>RONDLEIDING MET GIDS, UITGEBREIDE LUNCH, BOUW WORKSHOP</h2>
+  <div class="d-flex align-items-center">
+    <p class="description">Bedrijfsuitje - Groepsactiviteit - Bouw zoals de meesters</p>
+    <i class="fas fa-long-arrow-alt-right fa-2x arrow-left"></i>
+  </div>
+  
+</div>
+
+<hr class="divider-py mt-4">
+
+<div class="w-100 d-flex justify-content-center flex-column header-spacing">
+  <div class="d-flex align-items-center order-hover">
+    <h2>BESTEL UW TICKETS NU HIER</h2>
+    <i class="fas fa-long-arrow-alt-right fa-4x arrow-right"></i>
+  </div>
+</div>
+
+<hr class="divider-py mt-4">
+
+<div class="w-100 d-flex justify-content-center flex-column header-spacing">
+  <div class="program-container d-flex flex-column align-items-center">
+    <p class="description col-10 col-xl-9">
+      Voor groepen tot 200 personen kunt u kunst ervaren vanuit de ogen van kunstenaars. <br>
+      U zal bezig zijn met kunst en creativiteit. Het programma begint om 11:00 en eindigt om 15:00. Dit programma bestaat uit 3 onderdelen: de rondleiding met een gids, de uitgebreide lunch en de bouw workshop. <br>
+      Dit kost €100 per persoon en alles word geregeld door ons. <br>
+      U hoeft dus niks extra te betalen of mee te nemen.
+    </p>
+    <div class="d-flex flex-column  flex-xl-row-reverse align-items-center align-items-xl-start justify-content-xl-center py-5">
+      <div class="d-flex flex-column align-items-center align-items-xl-start col-10 col-xl-6">
+        <h3>RONDLEIDING MET GIDS</h3>
+        <p class="description col-12">
+          U begint met een tour door het museum met een gids die u alles over kunst en kunstenaars verteld.
+          Tijdens dit onderdeel kunt u foto’s maken van de kunst in het museum.
+          Dit kan handig zijn voor het laatste onderdeel.
+          Dus vergeet uw telefoon of camera niet zodat u foto’s kan maken van de kunst in het museum en kan gebruiken voor de workshop!
+        </p>
+      </div>
+      <div class="d-flex flex-column align-items-center align-items-xl-start col-10 col-xl-3">
+        <img class="artwork-1" src="<?php echo get_template_directory_uri(); ?>/images/artwork-guide.png" />
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 <!-- </ body> -->
